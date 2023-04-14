27713b277a0d4474682b32a56d8d97a43935ff5b
Colin Osborn <colin.osborn@gmail.com>
2023-04-13 19:31:48 -0600

    completed article component updates

--- app/javascript/components/Article.js
+++ app/javascript/components/Article.js
@@ -1,7 +1,9 @@
-import React from "react"
-import PropTypes from "prop-types"
+import React from "react";
+import PropTypes from "prop-types";
+import Timestamp from "react-timestamp";
+
 class Article extends React.Component {
-  render () {
+  render() {
     return (
       <React.Fragment>
         <div className="article-title">
@@ -10,9 +12,11 @@ class Article extends React.Component {
         <div className="article-body">
           {this.props.description}
           <div className="article-meta-details">
-            <small>Created by: {this.props.author}, {this.props.created_at} ago,
-                  last updated: {this.props.updated_at} ago
-            </small>  
+            <small>
+              Created by: {this.props.author},
+              <Timestamp time={this.props.created_at} precision={3} />, last
+              updated: <Timestamp time={this.props.updated_at} precision={3} />
+            </small>
           </div>
         </div>
       </React.Fragment>
@@ -26,7 +30,7 @@ Article.propTypes = {
   description: PropTypes.string,
   author: PropTypes.string,
   created_at: PropTypes.string,
-  updated_at: PropTypes.string
+  updated_at: PropTypes.string,
 };
 
-export default Article
+export default Article;
